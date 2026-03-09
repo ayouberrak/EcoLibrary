26a37fdbce50c13f81e043252ea2b48f4e09b552
ayouberrak <ayouberrak30@gmail.com>
2026-03-09 15:19:32 +0000

    feat(api): update API routes for Books and Auth

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\BookController;
+use App\Http\Controllers\Api\CategoryController;
 
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
+// Protected routes (Any authenticated user)
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/books', [BookController::class, 'index']);
+    Route::get('/books/{id}', [BookController::class, 'show']);
+    Route::get('/categories/{category_slug}/books/{book_slug}', [BookController::class, 'showBySlug']);
+    Route::get('/categories/{category}/books', [BookController::class, 'showByCategorie']);
+    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::get('/categories/{id}', [CategoryController::class, 'show']);
+    
+    Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::post('/register',[AuthController::class , 'register']);
-Route::post('/login',[AuthController::class , 'login']);
+    // Admin routes
+    Route::middleware('is_admin')->group(function () {
+        // Books management
+        Route::post('/books', [BookController::class, 'store']);
+        Route::put('/books/{id}', [BookController::class, 'update']);
+        Route::delete('/books/{id}', [BookController::class, 'destroy']);
 
-Route::middleware('auth:sanctum')->group(function(){
-    Route::post('/logout',[AuthController::class,'logout']);
+        // Categories management
+        Route::post('/categories', [CategoryController::class, 'store']);
+        Route::put('/categories/{id}', [CategoryController::class, 'update']);
+        Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+    });
 });
