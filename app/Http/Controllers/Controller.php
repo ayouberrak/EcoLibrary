49d15e306075c32759058bdc89882bb794902ed0
ayouberrak <ayouberrak30@gmail.com>
2026-03-09 14:26:51 +0000

    📝 Modifications du Controller principal pour la structure API

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
+use OpenApi\Attributes as OA;
 
+#[OA\Info(
+    title:'EcoLibrary',
+    version:'1.0.0',
+    description:'Api pour tester une library briaf'
+)]
+#[OA\Server(
+    url:'http://localhost:8000/api',
+    description:'serveur local'
+)]
+#[OA\SecurityScheme(
+    securityScheme:'sanctum',
+    type:'http',
+    scheme:'bearer',
+    bearerFormat:'JWT'
+)]
+#[OA\Security(security: [['sanctum' => []]])]
 abstract class Controller
 {
-    //
+
 }
